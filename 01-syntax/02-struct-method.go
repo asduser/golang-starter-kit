ee78d5f1cbe834ef88fc4faa44c21d9119fd2a20
Evgeniy <mailerboss00@mail.com>
2017-05-15 23:36:58 +0300

    struct-methods

---
+++ 01-syntax/02-struct-method.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Country struct {
+	Name string
+}
+
+type User struct {
+    Age int
+    Name string
+    Country Country
+}
+
+func (u User) greeting() string {
+    return fmt.Sprintf("Hi, my name is %s and I'm %d years old. I am from %s.", u.Name, u.Age, u.Country.Name)
+}
+
+func main() {
+	country := Country { "USA" }
+    user := User{ 30, "Bob", country }
+    fmt.Println(user.greeting());
+}
