01dafd8de5706be3ef0abdbf67cb7adbad4cb1ba
Evgeniy <mailerboss00@mail.com>
2017-06-13 00:09:50 +0300

    simple person -> man interface implementation

--- 01-syntax/05-interfaces.go
+++ 01-syntax/05-interfaces.go
@@ -2,8 +2,19 @@ package main
 
 import "fmt"
 
+type Person interface {
+    greeting(text string)
+}
+
+type Man struct {
+    Person
+}
+
+func (p *Man) greeting() {
+    fmt.Println("Hello, I'm a man!")
+}
+
 func main() {
-
-
-
+    p := new(Man)
+    p.greeting()
 }
